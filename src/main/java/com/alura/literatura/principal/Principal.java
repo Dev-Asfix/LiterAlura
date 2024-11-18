744d1e42d49ad55f0eb96ef80ea26f3c58db5dc3
Dev-Asfix <rosasramirez753@gmail.com>
2024-11-17 22:19:15 -0500

    creacion del menu

---
+++ src/main/java/com/alura/literatura/principal/Principal.java
@@ -0,0 +1,26 @@
+package com.alura.literatura;
+
+import com.alura.literatura.model.DatosLista;
+import com.alura.literatura.service.Conversion;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+
+    private Scanner in = new Scanner(System.in);
+    private final String URL_BASE = "";
+    Conversion conversion = new Conversion();
+    List<DatosLista> datosLista = new ArrayList<>();
+
+    public void menu(){
+        var opcion = 1;
+        while(opcion != 0){
+            var menu = """
+                    
+                    """
+        }
+    }
+
+}
